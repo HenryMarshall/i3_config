814eba1811d30ca673055e638970271195ceb038
hbaughman <hbaughman741@gmail.com>
2014-07-16 09:16:52 -0400

    add work mode and work mode flipped

--- bin/switch.py
+++ bin/switch.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python2.7
-import time
+from time import sleep
 import i3
-outputs = i3.get_outputs()
 
 # TODO: replace sleep kludge with some form of waiting
 
@@ -16,12 +15,13 @@ def find_active_workspace():
 active_workspace = find_active_workspace()
 
 # move everything
-for output in range(0, len(outputs)):
+outputs = i3.get_outputs()
+for output in range(0, 4):
     i3.workspace(outputs[output]['current_workspace'])
     i3.command('move', 'workspace to output up')
-    time.sleep(0.1)
+    sleep(0.1)
 
 # restore focus to previously focused
-time.sleep(0.1)
+sleep(0.1)
 if active_workspace:
     i3.command("workspace", active_workspace)
