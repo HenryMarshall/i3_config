a6ae9ebc71a0f163cbfc94843c16b9f75dda5722
hbaughman <hbaughman741@gmail.com>
2014-08-02 15:57:44 -0400

    Create flipper class

--- scripts/flip_screens.rb
+++ scripts/flip_screens.rb
@@ -1,7 +1,17 @@
 require 'gir_ffi'
 
-namespace = 'i3ipc'
-GirFFI.setup namespace
+class flipper
+  def initialize
+    GirFFI.setup 'i3ipc'
+    i3 = I3ipc::Connection.new(nil)
 
-i3 = I3ipc::Connection.new(nil)
-i3.command 'focus right'
+    @tree = i3.get_tree
+    @command = []
+  end
+
+  def move_workspace workspace
+    current_output = workspace.output.match(/\d/)[0].to_i
+    target_output = "xinerama-#{(current_output + 2) % 4}"
+    "workspace #{workspace.name}; move workspace to #{target_output}"
+  end
+end
