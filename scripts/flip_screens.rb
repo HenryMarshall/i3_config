867ffdbedf553da67f66f161bf83ce08d2973d0e
hbaughman <hbaughman741@gmail.com>
2014-08-02 17:44:03 -0400

    Refactor Flipper

--- scripts/flip_screens.rb
+++ scripts/flip_screens.rb
@@ -5,40 +5,56 @@ class Flipper
     GirFFI.setup 'i3ipc'
     @i3 = I3ipc::Connection.new(nil)
     @workspaces = @i3.get_workspaces
-    @tree = @i3.get_tree
-    @command = []
   end
 
-  def flip both=true
-    focused_workspace = nil
-    @workspaces.each do |workspace|
-      # If workspace was visible, put it on the top of the command stack so it
-      # will be visible after the move. Otherwise put it on the bottom.
-      if workspace.visible
-        @command.push move_workspace_command(workspace)
-        focused_workspace = workspace if workspace.focused
-      else
-        @command.unshift move_workspace_command(workspace)
-      end
-    end
+  def flip single_column=false
+    focused_workspace = @workspaces.find { |workspace| workspace.focused }
+    single_column ? (xinerama_number(workspace) % 2) : false
+    queue = build_move_queue(@workspaces, single_column)
+    # We focus where the originally focused workspace /will/ be (sync problems)
+    queue << "focus #{target_output(focused_workspace)}"
 
-    # Focus the output that the previously focused client /will/ be on when the
-    # movements end.
-    focused_output = focused_workspace.output.match(/\d/)[0].to_i
-    @command.push "focus xinerama-#{(focused_output + 2) % 4}"
-
-    # Send the entire command to i3 at once
-    @i3.command @command.join('; ')
+    @i3.command queue.join('; ')
   end
 
   private
 
-  def move_workspace_command workspace
-    current_output = workspace.output.match(/\d/)[0].to_i
-    target_output = "xinerama-#{(current_output + 2) % 4}"
-    "workspace #{workspace.name}; move workspace to #{target_output}"
+  def build_move_queue workspaces, single_column
+    queue = []
+    workspaces.each do |workspace|
+      add_to_queue(workspace, queue) if flip_this?(workspace, single_column)
+    end
+    queue
   end
+
+  def flip_this? workspace, single_column
+    xinerama_number(workspace) % 2 == single_column or not single_column
+  end
+
+  def add_to_queue workspace, queue
+    move_command = "workspace #{workspace.name};
+                    move workspace to #{target_output(workspace)}"
+
+    # If workspace was visible, put it on the top of the move queue so it
+    # will be visible after the move. Otherwise put it on the bottom.
+    if workspace.visible
+      queue.push(move_command)
+    else
+      queue.unshift(move_command)
+    end
+  end
+
+  def target_output workspace
+    # This assumes a 2x2 display grid and you are moving workspaces vertically.
+    # As we are using xinerama names are xinerama-0, xinerama-1 etc.
+    "xinerama-#{(xinerama_number(workspace) + 2) % 4}"
+  end
+
+  def xinerama_number workspace
+    workspace.output[-1].to_i
+  end
+
 end
 
 f = Flipper.new
-f.flip true
+f.flip
